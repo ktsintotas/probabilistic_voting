bbc7c7a7647ac6113628d842166083d416f2d64e
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 20:54:55 +0300

    Update geometricalCheck.m

--- matlab/geometricalCheck.m
+++ matlab/geometricalCheck.m
@@ -1,30 +1,29 @@
 % 
 
-% Copyright 2019, Konstantinos Tsintotas
+% Copyright 2020, Konstantinos A. Tsintotas
 % ktsintot@pme.duth.gr
 %
-% This file is part of Bag-of-Tracked-Words framework for loop closure
-% detection
+% This file is part of BoTW-LCD framework for visual loop closure detection
 %
-% Bag-of-Tracked-Words framework is free software: you can redistribute 
+% BoTW-LCD framework is free software: you can redistribute 
 % it and/or modify it under the terms of the MIT License as 
 % published by the corresponding authors.
 %  
-% Bag-of-Tracked-Words pipeline is distributed in the hope that it will be 
+% BoTW-LCD pipeline is distributed in the hope that it will be 
 % useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
 
-function [properImage, inliersTotal] = geometricalCheck(It, iBoTW, params, candidate, visualData)
+function [properImage, inliersTotal] = geometricalCheck(It, params, candidate, visualData)
      
     properImage = uint16(0);
     inliersTotal = uint16(0);    
     
-    indexPairs = matchFeatures(iBoTW.queryDescriptors{It}, visualData.features{candidate}, ...
+    indexPairs = matchFeatures(visualData.queryFeatures{It}, visualData.features{candidate}, ...
         'Unique', true, 'Method', 'Exhaustive', 'MatchThreshold', 10.0, 'MaxRatio', params.queryingDatabase.maxRatio);
 
-    if size(indexPairs, 1) >= params.queryingDatabase.inliersTheshold
-        matchedPoints1 = iBoTW.queryPoints{It}(indexPairs(:, 1), :);
+    if size(indexPairs, 1) >= 9
+        matchedPoints1 = visualData.queryPoints{It}.Location(indexPairs(:, 1), :);
         matchedPoints2 = visualData.points{candidate}.Location(indexPairs(:, 2), :);
         try
             [~, inliersIndex, ~] = estimateFundamentalMatrix(matchedPoints1, matchedPoints2, ...
