a09bf40d2ecea0614a81742ece50caaf67976a28
Konstantinos Tsintotas <ktsintot@pme.duth.gr>
2020-08-12 20:53:30 +0300

    Update main.m

--- matlab/main.m
+++ matlab/main.m
@@ -1,16 +1,15 @@
 % 
 
-% Copyright 2019, Konstantinos Tsintotas
+% Copyright 2020, Konstantinos A. Tsintotas
 % ktsintot@pme.duth.gr
 %
-% This file is part of Bag-of-Tracked-Words framework for loop closure
-% detection
+% This file is part of BoTW-LCD framework for visual loop closure detection
 %
-% Bag-of-Tracked-Words framework is free software: you can redistribute 
+% BoTW-LCD framework is free software: you can redistribute 
 % it and/or modify it under the terms of the MIT License as 
 % published by the corresponding authors.
 %  
-% Bag-of-Tracked-Words pipeline is distributed in the hope that it will be 
+% BoTW-LCD pipeline is distributed in the hope that it will be 
 % useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % MIT License for more details. <https://opensource.org/licenses/MIT>
@@ -30,10 +29,8 @@ visualData.frameRate = %;
 % timers memory allocation
 timer = timersInitialization(visualData, timer);
 
-% 1) the vocabulary build
-[BoTW, timer] = buildingDatabase(visualData, params, timer);
-% 2)  the query procedure
-[matches, BoTW, timer] = queryingDatabaseBaseline(params, visualData, BoTW, timer);
+% the query procedure
+[matches, timer] = queryingDatabase(params, visualData);
 
 % load the ground truth data for the corresponding dataset
 groundTruthMatrix = %;
